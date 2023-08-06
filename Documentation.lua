640cf21da7dafc9a4fa20c3d83dcd78bf2fa16c8
Crvstal8100 <99895952+Crvstal8100@users.noreply.github.com>
2023-08-06 16:05:57 +0200

    Update and rename README.md to Documentation.lua

---
+++ Documentation.lua
@@ -0,0 +1,106 @@
+-- Load UI Lib
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Crvstal8100/UILibrary/main/Library.lua"))()
+
+-- Create Menu
+local Menu = Library:Create("Title", "http://www.roblox.com/asset/?id=14299630623", "Description")
+
+--[[
+Create Menu
+arg1: string: Title
+arg2: string: Banner
+arg3: string: Description
+]]
+
+Menu:Alert("Text", function(option)
+    -- Continue = 1, Cancel = 2
+    print(option)
+end)
+
+--[[
+Send Alert
+arg1: string: Text
+arg2: callback: function
+]]
+
+Menu:Notify("Text", 5)
+
+--[[
+Send Notification
+arg1: string: Text
+arg2: number: Time
+]]
+
+local Tab = Menu:CreateTab("Title", "http://www.roblox.com/asset/?id=14299630623", "Description")
+
+--[[
+Create Tab
+arg1: string: Title
+arg2: string: Banner
+arg3: string: Description
+]]
+
+tab:CreateButton("Button", function()
+	  print("Ok")
+end)
+
+--[[
+Create Button
+arg1: string: Text
+arg2: callback: function
+]]
+
+tab:CreateToggle("Toggle", false, function(state)
+	  print(state)
+end)
+
+--[[
+Create Toggle
+arg1: string: Text
+arg2: bool: State
+arg3: callback: function
+]]
+
+tab:CreateSlider("Slider", 0, 10, function(value)
+	  print(value)
+end)
+
+--[[
+Create Slider
+arg1: string: Text
+arg2: number: MinValue
+arg3: number: MaxValue
+arg4: callback: function
+]]
+
+tab:CreateList("List", {"1", "2", "3"}, function(value)
+	  print(value)
+end)
+
+--[[
+Create List
+arg1: string: Text
+arg2: table: List
+arg3: callback: function
+]]
+
+tab:CreateKeybind("Keybind", "K", function()
+	  print("Key Pressed")
+end)
+
+--[[
+Create Keybind
+arg1: string: Text
+arg2: string: Keybind
+arg3: callback: function
+]]
+
+tab:CreateTextbox("Textbox", ". . .", function(text)
+	  print(text)
+end)
+
+--[[
+Create Textbox
+arg1: string: Text
+arg2: string: placeholder
+arg3: callback: function
+]]
